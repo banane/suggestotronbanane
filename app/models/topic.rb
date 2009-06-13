5a02b0bc2aa5f027af2d20cbb71d9e01dce48057
Anna <banane@gmail.com>
2009-06-13 15:55:40 -0700

    final upload

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,5 @@
 class Topic < ActiveRecord::Base
+  has_many :votes
+
+  validates_presence_of :title
 end
---
+++ app/models/vote.rb
@@ -0,0 +1,6 @@
+class Vote < ActiveRecord::Base
+	belongs_to :topic
+
+	validates_presence_of :topic
+
+end
